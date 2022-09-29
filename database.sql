4e4f6e280e4317ade0fc203ec4ab3b2eca2e1f97
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-09-29 14:36:15 -0500

    added DB SQL

--- database.sql
+++ database.sql
@@ -1,10 +1,39 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "admin" BOOLEAN DEFAULT FALSE,
+    "18+" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE "category" (
+    "id" SERIAL PRIMARY KEY,
+    "categoryName" VARCHAR (100) NOT NULL
+);
+
+INSERT INTO "category" ("categoryName")
+    VALUES ('Political/Legal');
+INSERT INTO "category" ("categoryName")
+    VALUES ('Medical/Scientific');
+INSERT INTO "category" ("categoryName")
+    VALUES ('Business/Cultural');
+
+CREATE TABLE "timeline" (
+    "id" SERIAL PRIMARY KEY,
+    "title" VARCHAR (100) NOT NULL,
+    "date" DATE NOT NULL,
+    "image" VARCHAR (1000),
+    "info" VARCHAR (65535),
+    "references" VARCHAR (10000),
+    "category_id" INT REFERENCES "category",
+    "authorized" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE "stories" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user",
+    "story" VARCHAR (65535) NOT NULL,
+    "authorized" BOOLEAN DEFAULT FALSE,
+    "timeline_id" INT REFERENCES "timeline"
+
 );
