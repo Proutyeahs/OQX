2f5a09076d717e1370d06cffbf88b270162ed33c
emsweetland <emmaksweetland@gmail.com>
2022-09-30 14:07:21 -0500

    mui icons in nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,6 +3,19 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+//MUI stuff here
+// import Button from '@mui/material/Button';
+import { IconButton } from '@mui/material';
+ import MenuRoundedIcon from '@mui/icons-material/MenuRounded'; //burger menu
+ import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined'; //back button
+import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined'; //political/legal timeline
+ import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined'; //business/cultural timeline
+ import LocalHospitalOutlinedIcon from '@mui/icons-material/LocalHospitalOutlined'; //health/science timeline 
+
+
+
+
+
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -10,8 +23,40 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">OQX</h2>
       </Link>
+      
+      <Link className="navLink" to="/user">
+        <IconButton aria-label="back">
+          <ArrowBackOutlinedIcon />
+        </IconButton>
+      </Link>
+
+
+      <Link className="navLink" to="/user">
+        <IconButton aria-label="political">
+          <AccountBalanceOutlinedIcon />
+        </IconButton>
+      </Link>
+
+      <Link className="navLink" to="/user">
+        <IconButton aria-label="health">
+          <LocalHospitalOutlinedIcon />
+        </IconButton>
+      </Link>
+
+      <Link className="navLink" to="/user">
+        <IconButton aria-label="business">
+          <BusinessOutlinedIcon />
+        </IconButton>
+      </Link>
+
+      <Link className="navLink" to="/user">
+        <IconButton aria-label="menu">
+          <MenuRoundedIcon />
+        </IconButton>
+      </Link>
+
       <div>
         {/* If no user is logged in, show these links */}
         {!user.id && (
@@ -24,6 +69,10 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
+
+
+
+
             <Link className="navLink" to="/user">
               Home
             </Link>
