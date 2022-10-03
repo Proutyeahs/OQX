6f52b0dc373bcadb1e8e073414dc605a6b0e2902
Tim Anderson <t.anderson0085@gmail.com>
2022-10-03 13:40:39 -0500

    added css page and text to AboutPage

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import './AboutPage.css';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -8,8 +15,17 @@ import React from 'react';
 function AboutPage() {
   return (
     <div className="container">
+      <h1>Our Queer Hxstory</h1>
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>
+          LGBTQ literature and educational resources are often barred behind paywalls and complex language.
+          <br></br>
+          Our Queer Hxstory creates a platform for people to access three educational timelines: political/legal, medical/scientific, and business/cultural. All at no cost and accessible for people as young as ten.
+          <br></br>
+          Now, the LGBTQ story is available to all reading levels and all educational levels. OQX gives you clean timelines that you can click into and explore the consequential events that still impact us all today. Whether you are discovering queer history for the first time, or diving deep into the research, Our Queer Hxstory opens the doors for you. 
+          <br></br>
+          <a href="https://ourqueerhxtory.com">Please Consider Donating to OQX</a>
+</p>
       </div>
     </div>
   );
