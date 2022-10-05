23b7855ecbbffe6f75a5ba38b994f90c4ef5104e
Kas Eam <KassandraEam@gmail.com>
2022-10-05 14:14:30 -0500

    Added the other timelines.

--- src/components/BusinessCultural/BusinessCultural.jsx
+++ src/components/BusinessCultural/BusinessCultural.jsx
@@ -2,24 +2,66 @@ import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from 'react';
 
 function BusinessCultural() {
-
     const dispatch = useDispatch();
 
+    // Created a variable that contains CSS to then style the timeline itself.
+    const divStyle = {
+        left: '5%',
+        // right: '50%',
+        border: '3px solid yellow',
+        borderRadius: '1%'
+    };
+    
+    //formatDate is used to make our timestamps pretty
+    const formatDate = (dateString) => {
+        const options = { month: "long", day: "numeric", year: 'numeric' }
+        return new Date(dateString).toLocaleDateString(undefined, options)
+    }
     const events = useSelector((store) => store.event)
 
-    // get the event data on page load/reload
     useEffect(() => {
         dispatch({
-          type: 'GET_EVENT',
-          payload: 3
+            type: 'GET_EVENT',
+            payload: 3
         })
-      }, [])
+    }, [])
 
     return (
         <>
-            {events.map(event => (
-                <p>{ event.title }</p>
-            ))}
+            <section>
+                {/* This first chunk of DIVs contains the header for the page.*/}
+                <div className="bg-white text-black">
+                    <div className="container mx-auto flex flex-col items-start md:flex-row md:my-24">
+                        <div className="flex flex-col w-full sticky md:top-36 lg:w-1/3 md:mt-12 px-8">
+                            <p className="text-gray-900 uppercase tracking-loose w-30">Business and Cultural</p>
+                            <p className="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">Working Process of Fest</p>
+                            <p className="text-sm md:text-base text-gray-900 mb-4">
+                                Something about Business and Cultural stuff.
+                            </p>
+                        </div>
+                        {/* This second chunk of DIVs contains the card for each individual item from the DB for the respective timeline*/}
+                        <div className="ml-0 md:ml-12 lg:w-2/3 sticky">
+                            <div className="relative wrap overflow-hidden p-10 h-full">
+                                <div className="border-2-2 border-blue-555 absolute h-full border" style={divStyle}></div>
+                                <div className=''>
+                                    {events.map(event => (
+                                        <>
+                                            {event.image != '' &&
+                                                <img className="rounded-t-lg" src={event.image} />}
+                                            <div className="mb-10 px-6 py-4 text-left max-w-sm rounded-b-lg overflow-hidden shadow-2xl" key={event.id}>
+                                                <p className="font-bold text-xl mb-2">{event.title}</p>
+                                                <p className="text-gray-700 text-base">Date:{event.date}</p>
+                                                <p className="text-gray-700 text-base">Info:{event.info}</p>
+                                            </div>
+                                        </>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section >
         </>
     )
 }
--- src/components/PoliticalLegal/PoliticalLegal.jsx
+++ src/components/PoliticalLegal/PoliticalLegal.jsx
@@ -6,21 +6,60 @@ function PoliticalLegal() {
 
     const dispatch = useDispatch();
 
+    // Created a variable that contains CSS to then style the timeline itself.
+    const divStyle = {
+        left: '5%',
+        // right: '50%',
+        border: '3px solid blue',
+        borderRadius: '1%'
+    };
+
     const events = useSelector((store) => store.event)
 
-    // get the event data on page load/reload
     useEffect(() => {
         dispatch({
-          type: 'GET_EVENT',
-          payload: 1
+            type: 'GET_EVENT',
+            payload: 1
         })
-      }, [])
+    }, [])
 
     return (
         <>
-            {events.map(event => (
-                <p>{ event.title }</p>
-            ))}
+            <section>
+                {/* This first chunk of DIVs contains the header for the page.*/}
+                <div className="bg-white text-black">
+                    <div className="container mx-auto flex flex-col items-start md:flex-row md:my-24">
+                        <div className="flex flex-col w-full sticky md:top-36 lg:w-1/3 md:mt-12 px-8">
+                            <p className="text-gray-900 uppercase tracking-loose w-30">Political Legal</p>
+                            <p className="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">Working Process of Fest</p>
+                            <p className="text-sm md:text-base text-gray-900 mb-4">
+                                Something about Political Legal stuff.
+                            </p>
+                        </div>
+                        {/* This second chunk of DIVs contains the card for each individual item from the DB for the respective timeline*/}
+                        <div className="ml-0 md:ml-12 lg:w-2/3 sticky">
+                            <div className="relative wrap overflow-hidden p-10 h-full">
+                                <div className="border-2-2 border-blue-555 absolute h-full border" style={divStyle}></div>
+                                <div className=''>
+                                    {events.map(event => (
+                                        <>
+                                            {event.image != '' &&
+                                                <img className="rounded-t-lg" src={event.image} />}
+                                            <div className="mb-10 px-6 py-4 text-left max-w-sm rounded-b-lg overflow-hidden shadow-2xl" key={event.id}>
+                                                <p className="font-bold text-xl mb-2">{event.title}</p>
+                                                <p className="text-gray-700 text-base">Date:{formatDate(event.date)}
+                                                </p>
+                                                <p className="text-gray-700 text-base">Info:{event.info}</p>
+                                            </div>
+                                        </>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section >
         </>
     )
 }
