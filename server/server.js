ef4f844f0cccccdce936ea456da541deb3834629
emsweetland <emmaksweetland@gmail.com>
2022-10-08 14:23:27 -0500

    GET resources from DB

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require('./strategies/user.strategy');
 const userRouter = require('./routes/user.router');
 const eventRouter = require('./routes/event.router');
 const storiesRouter = require('./routes/stories.router')
+const resourceRouter = require('./routes/resource.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -26,7 +27,8 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/event', eventRouter);
-app.use('/api/stories', storiesRouter)
+app.use('/api/stories', storiesRouter);
+app.use('/api/resource', resourceRouter);
 
 // Serve static files
 app.use(express.static('build'));
---
+++ src/redux/reducers/resource.reducer.js
@@ -0,0 +1,11 @@
+// holds the resource list
+const resourceReducer = (state = [], action) => {
+    switch (action.type) {
+      case 'SET_RESOURCE':
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+  export default resourceReducer;
