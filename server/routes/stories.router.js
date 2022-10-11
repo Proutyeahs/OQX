b77206f4918fc39543dd5a05995874d6e5e18188
Eric <103910650+Proutyeahs@users.noreply.github.com>
2022-10-11 10:01:44 -0500

    Update stories.router.js

--- server/routes/stories.router.js
+++ server/routes/stories.router.js
@@ -11,10 +11,10 @@ router.post('/', rejectUnauthenticated, (req, res) => {
     console.log('Stories.router.js POST route:', req.body)
     const query = `
     INSERT INTO "stories" ("user_id", "story", "authorized","timeline_id")
-    VALUES ($1, $2, false, $3)
+    VALUES ($1, $2, $4, $3)
     ;`;
     // "displayName", 
-    pool.query(query, [req.body.user_id, req.body.story, req.body.timelineEvent]).
+    pool.query(query, [req.body.user_id, req.body.story, req.body.timelineEvent, 'false']).
         then(result => {
             res.sendStatus(200)
         })
@@ -24,7 +24,7 @@ router.post('/', rejectUnauthenticated, (req, res) => {
         })
 });
 
-router.get('/', rejectUnauthenticated, (req, res) => {
+router.get('/', rejectUnauthenticated, rejectUnauthenticatedAdmin, (req, res) => {
     console.log("stories", req.params)
     const query = `
     SELECT "title", "stories".* FROM "timeline"
@@ -43,14 +43,11 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 
 router.get('/:id', (req, res) => {
     console.log("stories", req.params)
-    const queryPayload = req.params.id;
     const query = `
-    SELECT "title", "stories".* FROM "timeline"
-    JOIN "stories"
-    ON "stories".timeline_id = "timeline".id
-    WHERE "stories".authorized = true AND $1 = "timeline".id
+    SELECT * FROM "stories"
+    WHERE ("stories".authorized = $2 AND $1 = "timeline_id")
     ;`;
-    pool.query(query, [queryPayload])
+    pool.query(query, [req.params.id, 'true'])
         .then(result => {
             console.log("userStories", result.rows)
             res.send(result.rows)
@@ -61,14 +58,14 @@ router.get('/:id', (req, res) => {
         })
 });
 
-router.put('/:id', rejectUnauthenticated, (req, res) => {
+router.put('/:id', rejectUnauthenticated, rejectUnauthenticatedAdmin, (req, res) => {
     console.log(req.params.id)
     const query = `
     UPDATE "stories"
-        SET "authorized" = true
+        SET "authorized" = $2
         WHERE "id" = $1
     ;`;
-    pool.query(query, [req.params.id]).then(result => {
+    pool.query(query, [req.params.id, 'true']).then(result => {
         res.sendStatus(200)
     }).catch(err => {
         console.log(err)
@@ -76,7 +73,7 @@ router.put('/:id', rejectUnauthenticated, (req, res) => {
     })
 });
 
-router.delete('/:id', rejectUnauthenticated, (req, res) => {
+router.delete('/:id', rejectUnauthenticated, rejectUnauthenticatedAdmin, (req, res) => {
     const query = `
         DELETE FROM "stories"
         WHERE "id" = $1
