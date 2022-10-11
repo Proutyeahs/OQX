6fc0b04f1f9e34a2f782dcc98ec44ba08a256ca7
Kas Eam <KassandraEam@gmail.com>
2022-10-11 11:32:06 -0500

    Search bar now searches upon pressing enter and clears after enter as well

--- src/components/BusinessCultural/BusinessCultural.jsx
+++ src/components/BusinessCultural/BusinessCultural.jsx
@@ -9,7 +9,7 @@ function BusinessCultural() {
     // const completion = useReadingProgress();
     const dispatch = useDispatch();
     const history = useHistory();
-    const [search, setSearch] = useState()
+    const [search, setSearch] = useState('')
 
     const handleClick = (id) => {
         console.log('Handle Click');
@@ -17,15 +17,17 @@ function BusinessCultural() {
         history.push(`/eventdetail/${id}`)
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
         console.log('Search input: ', search);
+        e.preventDefault();
         dispatch({
             type: 'GET_SEARCHED_EVENTS',
-            payload: { 
-                payload: search,
+            payload: {
+                payload: ('%' + search + '%'),
                 category: 3
             }
         })
+        setSearch('')
     }
     const formatDate = (dateString) => {
         const options = { month: "long", day: "numeric", year: 'numeric' }
@@ -60,8 +62,10 @@ function BusinessCultural() {
         <>
             <section>
                 {/* This first chunk of DIVs contains the header for the page.*/}
-                <input type="text" placeholder="Search.. on submit" onChange={(event) => setSearch('%' + event.target.value + '%')}></input>
-                <button onClick={handleSubmit}>Submit</button>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Search.. on submit" value={search} onChange={(e) => setSearch(e.target.value)}></input>
+                    <button onClick={handleSubmit}>Submit</button>
+                </form>
                 <div className="bg-white text-black">
                     <div className="container mx-auto flex flex-col items-start md:flex-row md:my-24">
                         <div className="flex flex-col w-full sticky md:top-36 lg:w-1/3 md:mt-12 px-8">
