c6bd28b203ca1f2f3f24d6083f879895e6fc8e54
Kas Eam <KassandraEam@gmail.com>
2022-10-12 09:43:10 -0500

    Shortened descriptions on cards on the timeline

--- src/components/PoliticalLegal/PoliticalLegal.jsx
+++ src/components/PoliticalLegal/PoliticalLegal.jsx
@@ -13,8 +13,17 @@ function PoliticalLegal() {
     const dispatch = useDispatch();
     const history = useHistory();
     const [search, setSearch] = useState('')
+    const [shortDescription, setShortDescription] = useState('');
+    
+    // const test = (action) => {
+    //     const split = action.split(' ').slice(0,10).join(' ');
+    //     console.log(split);
+    // }
 
-
+    const test = (test) => {
+    return (test.split(' ').slice(0,10).join(' '));
+    }
+    
     const handleClick = (id) => {
         console.log('Handle Click');
         console.log('ID', id)
@@ -106,7 +115,7 @@ function PoliticalLegal() {
                                                     <img className="rounded-t-lg" src={event.image} />}
                                                 <div className="mb-10 px-6 py-4 text-left max-w-sm rounded-b-lg overflow-hidden shadow-xl" key={event.id}>
                                                     <p className="font-bold text-xl mb-2">{event.title}</p>
-                                                    <p className="text-gray-700 text-base">{event.info}</p>
+                                                    <p className="text-gray-700 text-base">{test(event.info)}...</p>
                                                 </div>
                                             </div>
                                         </div >
