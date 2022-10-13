ec5fb8363a52da3f52071142a07bb6d8c097b39f
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-10-13 15:17:15 -0500

    ordered admin dash

--- src/components/AdminDash/AdminDash.jsx
+++ src/components/AdminDash/AdminDash.jsx
@@ -11,23 +11,7 @@ function AdminDash() {
     return (
         <div className="dash">
             <h1>Admin Dashboard</h1>
-            <h1>Create</h1>
-   
-            <Button className="link" variant="contained" color="success" onClick={() => history.push('/eventForm')}>
-                Create Event
-            </Button>
-
-            <Button className="link" variant="contained" color="success" onClick={() => history.push('/sponsorForm')}>
-                Add Sponsor
-            </Button>
-
-            <Button className="link" variant="contained" color="success" onClick={() => history.push('/resourceForm')}>
-                Add Resource
-            </Button>
-
-            <h2>Review</h2>
-            <h6>Approve events and user stories,</h6>
-            <h6>edit and delete sponsors and resources</h6>
+            <h1>Review</h1>
 
             <Button className="link" variant="contained" onClick={() => history.push('/eventReview')}>
                 Review Events
@@ -35,6 +19,16 @@ function AdminDash() {
 
             <Button className="link" variant="contained" onClick={() => history.push('/userStoriesReview')}>
                 Review Stories
+            </Button>
+
+            <h2>Create</h2>
+
+            <Button className="link" variant="contained" color="success" onClick={() => history.push('/resourceForm')}>
+                Add Resource
+            </Button>
+
+            <Button className="link" variant="contained" color="success" onClick={() => history.push('/sponsorForm')}>
+                Add Sponsor
             </Button>
 
             <Button className="link" variant="contained" onClick={() => history.push('/resourceReview')}>
