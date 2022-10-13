8413cf4fdb2a0a738d5397eb50b27e38118b92fe
emsweetland <emmaksweetland@gmail.com>
2022-10-13 11:48:46 -0500

    rendering tiers on page

--- src/components/SponsorsPage/SponsorsPage.jsx
+++ src/components/SponsorsPage/SponsorsPage.jsx
@@ -21,76 +21,120 @@ function SponsorsPage() {
         })
     }, [])
 
-    //console.log(sponsors)
-    //TODO: loop and organize sponsors by tier
-    const tier1 = () => {
+
+    let gold = []
+    let silver = []
+    let bronze = []
+    const sort = () => {
         for (let sponsor of sponsors) {
             console.log(sponsor)
-            if (sponsor.levelOfDonation === 1) {
-                return <ul key={sponsor.id}>
-                    <li>{sponsor.company}</li>
-                    <img src={sponsor.image} />
-                </ul>
-            }
+        
+        if (sponsor.levelOfDonation === 1) {
+            gold.push(sponsor);
         }
-    } //end tier1
-
-    const tier2 = () => {
-        for (let sponsor of sponsors) {
-            console.log(sponsor)
-            if (sponsor.levelOfDonation === 2) {
-                return <ul key={sponsor.id}>
-                    <li>{sponsor.company}</li>
-                    <img src={sponsor.image} />
-                </ul>
-            }
+        if (sponsor.levelOfDonation === 2) {
+            silver.push(sponsor);
         }
-    } //end tier2
+        if (sponsor.levelOfDonation === 3) {
+            bronze.push(sponsor);
+        } 
+    } return console.log (gold, silver, bronze)
+}
 
-    const tier3 = () => {
-        for (let sponsor of sponsors) {
-            console.log(sponsor)
-            if (sponsor.levelOfDonation === 3) {
-                return <ul key={sponsor.id}>
-                    <li>{sponsor.company}</li>
-                    <img src={sponsor.image} />
-                </ul>
-            }
+sort()
+
+
+//console.log(sponsors)
+//TODO: loop and organize sponsors by tier
+const tier1 = () => {
+    for (let sponsor of sponsors) {
+        console.log(sponsor)
+        if (sponsor.levelOfDonation === 1) {
+            return <ul key={sponsor.id}>
+                <li>{sponsor.company}</li>
+                <img src={sponsor.image} />
+            </ul>
         }
-    } //end tier3
+    }
+} //end tier1
+
+const tier2 = () => {
+    for (let sponsor of sponsors) {
+        console.log(sponsor)
+        if (sponsor.levelOfDonation === 2) {
+            return <ul key={sponsor.id}>
+                <li>{sponsor.company}</li>
+                <img src={sponsor.image} />
+            </ul>
+        }
+    }
+} //end tier2
+
+const tier3 = () => {
+    for (let sponsor of sponsors) {
+        console.log(sponsor)
+        if (sponsor.levelOfDonation === 3) {
+            return <ul key={sponsor.id}>
+                <li>{sponsor.company}</li>
+                <img src={sponsor.image} />
+            </ul>
+        }
+    }
+} //end tier3
 
 
 
 
 
-    return (
-        <>
+return (
+    <>
+        <div>
+            <h2>{heading}</h2>
+        </div>
+
+        <div >
+            <a className="text-gray-300" href="https://atelierlks.com/stunning-examples-fake-logos/">source for images used</a>
+        </div>
+
+        <div class="bg-cyan-300">
+            tier 1<br></br>
             <div>
-                <h2>{heading}</h2>
+                {gold.map(sponsor =>
+                    <ul key={sponsor.id}>
+                        <li>{sponsor.company}</li>
+                        <img src={sponsor.image} />
+                        <li>{sponsor.levelOfDonation}</li>
+                    </ul>)}
             </div>
+        </div>
 
-            <div >
-                <a className="text-gray-300" href="https://atelierlks.com/stunning-examples-fake-logos/">source for images used</a>
+        <div class="bg-cyan-400">
+            tier 2<br></br>
+            <div>
+                {silver.map(sponsor =>
+                    <ul key={sponsor.id}>
+                        <li>{sponsor.company}</li>
+                        <img src={sponsor.image} />
+                        <li>{sponsor.levelOfDonation}</li>
+                    </ul>)}
             </div>
+        </div>
 
-            <div class="bg-cyan-300">
-                tier 1<br></br>
-                {tier1()}
+        <div class="bg-cyan-500">
+            tier 3<br></br>
+            <div>
+                {bronze.map(sponsor =>
+                    <ul key={sponsor.id}>
+                        <li>{sponsor.company}</li>
+                        <img src={sponsor.image} />
+                        <li>{sponsor.levelOfDonation}</li>
+                    </ul>)}
             </div>
-            
-            <div class="bg-cyan-400">
-                tier 2<br></br>
-                {tier2()}
-            </div>
-
-            <div class="bg-cyan-500">
-                tier 3<br></br>
-                {tier3()}
-            </div>
+        </div>
 
 
 
-            {/* <div className="sponsor">
+        {/* <div className="sponsor">
                 {sponsors.map(sponsor =>
                     <ul key={sponsor.id}>
                         <li>{sponsor.company}</li>
@@ -98,8 +142,8 @@ function SponsorsPage() {
                         <li>{sponsor.levelOfDonation}</li>
                     </ul>)}
             </div> */}
-        </>
-    );
+    </>
+);
 }
 
 export default SponsorsPage;
