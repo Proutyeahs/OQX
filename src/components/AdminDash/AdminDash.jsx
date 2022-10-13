41b71d538033431429989cac44dc79bf747e87ef
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-10-13 15:28:25 -0500

    outlined buttons

--- src/components/AdminDash/AdminDash.jsx
+++ src/components/AdminDash/AdminDash.jsx
@@ -13,29 +13,29 @@ function AdminDash() {
             <h1>Admin Dashboard</h1>
             <h1>Review</h1>
 
-            <Button className="link" variant="contained" onClick={() => history.push('/eventReview')}>
+            <Button className="link" variant="outlined" onClick={() => history.push('/eventReview')}>
                 Review Events
             </Button>
 
-            <Button className="link" variant="contained" onClick={() => history.push('/userStoriesReview')}>
+            <Button className="link" variant="outlined" onClick={() => history.push('/userStoriesReview')}>
                 Review Stories
             </Button>
 
             <h2>Create</h2>
 
-            <Button className="link" variant="contained" color="success" onClick={() => history.push('/resourceForm')}>
+            <Button className="link" variant="outlined" color="success" onClick={() => history.push('/resourceForm')}>
                 Add Resource
             </Button>
 
-            <Button className="link" variant="contained" color="success" onClick={() => history.push('/sponsorForm')}>
+            <Button className="link" variant="outlined" color="success" onClick={() => history.push('/sponsorForm')}>
                 Add Sponsor
             </Button>
-
-            <Button className="link" variant="contained" onClick={() => history.push('/resourceReview')}>
+            <br></br>
+            <Button className="link" variant="outlined" onClick={() => history.push('/resourceReview')}>
                 Review Resources
             </Button>
 
-            <Button className="link" variant="contained" onClick={() => history.push('/sponsorReview')}>
+            <Button className="link" variant="outlined" onClick={() => history.push('/sponsorReview')}>
                 Review Sponsors
             </Button>
 
